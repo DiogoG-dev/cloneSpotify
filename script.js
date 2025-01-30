f5e9ca1133b91945395d350dc473a847ea5947c8
DiogoGabriel <diogogabriel.tech@gmail.com>
2025-01-30 20:40:42 -0300

    Adding search artists and card artists

---
+++ script.js
@@ -0,0 +1,44 @@
+const searchInput = document.getElementById('search-input');
+const resultArtist = document.getElementById("result-artist");
+const resultPlaylist = document.getElementById('result-playlists');
+
+function requestApi(searchTerm) {
+    const url = `http://localhost:3000/artists?name_like=${searchTerm}`;
+    fetch(url)
+        .then((response) => response.json())
+        .then(result => filterResults(result, searchTerm))
+        .catch(error => console.error("Erro na requisição:", error));
+}
+
+function filterResults(result, searchTerm) {
+    // Filtra os artistas que começam com a palavra pesquisada
+    const filteredArtists = result.filter(artist => 
+        artist.name.toLowerCase().startsWith(searchTerm.toLowerCase())
+    );
+
+    displayResults(filteredArtists);
+}
+
+function displayResults(result) {
+    resultPlaylist.classList.add('hidden');
+    const artistName = document.getElementById('artist-name');
+    const artistImage = document.getElementById('artist-img');
+
+    result.forEach(element => {
+        artistName.innerText = element.name;
+        artistImage.src = element.urlImg;
+    });
+
+    resultArtist.classList.remove('hidden');
+}
+
+document.addEventListener('input', function () {
+    const searchTerm = searchInput.value.toLowerCase();
+    if (searchTerm === '') {
+        resultPlaylist.classList.add('hidden');
+        resultArtist.classList.remove('hidden');
+        return
+    }
+    
+    requestApi(searchTerm);
+})
