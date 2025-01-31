5512bc5314df7a89fd1f8005aa59ac5607d0a5e4
DiogoGabriel <diogogabriel.tech@gmail.com>
2025-01-31 20:56:22 -0300

    Adding component footer

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './components/Vars.css';
 import Header from './components/Header/Header';
 import Sidebar from './components/Sidebar/Sidebar';
 import Main from './components/Main/Main';
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
     <Header/>
     <Sidebar/>
     <Main/>
+    <Footer/>
     </>
   );
 }
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,15 @@
+import './Footer.css';
+
+const Footer = () => {
+    return (
+        <footer>
+            <div className="footer-text">
+                <span>TESTE O PREMIUM DE GRAÇA</span>
+                <span>Inscreva-se para curtir músicas ilimitadas e podcasts só com alguns anúncios. Não precisa de cartão de crédito.</span>
+            </div>
+            <button className="footer-button">Inscreva-se grátis</button>
+        </footer>
+    )
+}
+
+export default Footer;
