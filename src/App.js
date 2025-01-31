a3d652122f8746cc3fbbf42c65970c869737f4f4
DiogoGabriel <diogogabriel.tech@gmail.com>
2025-01-31 20:55:24 -0300

    Adding component header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import './components/Vars.css';
+import Header from './components/Header/Header';
 import Sidebar from './components/Sidebar/Sidebar';
 
 function App() {
   return (
     <>
+    <Header/>
     <Sidebar/>
     </>
   );
