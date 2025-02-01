bbca6243745305b19a3c65e4e1f3c6aeb367e643
DiogoGabriel <diogogabriel.tech@gmail.com>
2025-01-31 21:03:40 -0300

    Adding the artist search function

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import './components/Vars.css';
+import './SearchArtists';
 import Header from './components/Header/Header';
 import Sidebar from './components/Sidebar/Sidebar';
 import Main from './components/Main/Main';
